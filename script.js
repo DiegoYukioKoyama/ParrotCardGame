35bd8278d796141e02148574728979700913d970
DiegoYukioKoyama <yukioko.jp@gmail.com>
2022-10-24 00:16:06 -0300

    acerto e erro

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 let contador = 0;
 let nCarta;
+let cartaVirada1 = '';
+let cartaVirada2 = '';
 
 const cartaFront = [
     "<img src='imagens/bobrossparrot.gif'>",
@@ -16,25 +18,71 @@ numeroCartas();
 
 function virar(elemento){
 
-    elemento.classList.toggle("virada");
+        if(elemento.className.includes('virada')){
+    
+            return;
+    
+        }
+
+        if(cartaVirada1 === ''){
+
+        elemento.classList.add("virada");
+        cartaVirada1 = elemento;
+        
+    
+        }
+        
+        else if(cartaVirada2 === ''){
+
+            elemento.classList.add("virada");
+            cartaVirada2 = elemento;
+            
+        }
+
+        if(cartaVirada1 !== '' && cartaVirada2 !== ''){
+
+            if(cartaVirada1.innerHTML === cartaVirada2.innerHTML){
+
+                cartaVirada1 = "";
+                cartaVirada2 = "";
+
+            } else{
+
+                setTimeout(function(){
+
+                    cartaVirada1.classList.remove("virada");
+                    cartaVirada2.classList.remove("virada");
+
+                    cartaVirada1 = "";
+                    cartaVirada2 = "";
+
+                }, 1000);
+
+            }
+            
+
+        }
 
 }
+
+
 
 function adicionarCarta(){
 
     const cartas = document.querySelector(".container-cartas")
 
-        const carta = `<div onclick="virar(this)" class="carta">
+    const carta = `<div onclick="virar(this)" class="carta">
 
-            <div class="carta-front face">
-                ${cartaFront[i]}
-            </div>
+        <div class="carta-front face">
+            ${cartaFront[i]}
+        </div>
     
-            <div class="carta-back face">
-                <img src="imagens/back.png">
-            </div>
+        <div class="carta-back face">
+            <img src="imagens/back.png">
+        </div>
     
-        </div>`
+    </div>`
+
     cartas.innerHTML += carta;
 
 }
